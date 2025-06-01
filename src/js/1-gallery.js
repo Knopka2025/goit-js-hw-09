b943b7643db38bfe1f71de17a0fa85f987976a00
Vlada <geazint6@gmail.com>
2025-06-01 13:36:25 +0300

    add HW

---
+++ src/js/1-gallery.js
@@ -0,0 +1,94 @@
+const images = [
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+      description: 'Квітка Хоккайдо',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+      description: 'Контейнерні перевезення',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+      description: 'Вид на пляж з висоти пташиного польоту',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+      description: 'Цвітіння квітів',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+      description: 'Альпійські гори',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+      description: 'Плавання під парусом у горах',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+      description: 'Альпійські весняні луки',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+      description: 'Природничий пейзаж',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+      description: 'Маяк на березі моря',
+    },
+  ];
+  
+  const gallery = document.querySelector('.gallery');
+  
+  const imageGallary = images
+    .map(
+      image =>
+        `<li class="gallery-item">
+  <a class="gallery-link" href="${image.original}">
+      <img
+      class="gallery-image"
+      src="${image.preview}"
+      alt="${image.description}"
+      />
+  </a>
+  </li>`
+    )
+    .join(``);
+  
+  gallery.insertAdjacentHTML('afterbegin', imageGallary);
+  import SimpleLightbox from 'simplelightbox';
+  
+  new SimpleLightbox('.gallery a', {
+    overlay: true,
+    overlayOpacity: 0.8,
+    captionDelay: 250,
+    captions: true,
+    captionsData: 'alt',
+    captionClass: 'captions',
+  });
